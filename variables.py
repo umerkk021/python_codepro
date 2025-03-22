0006d69b4f313186910bab8b16b3de4f7fa6bccd
umerkk021 <umerkk021@gmail.com>
2025-03-22 15:56:54 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,32 @@
+# name = "codepro"
+# num1 = 15
+# volume = 12.0
+# boolean = False
+
+# print( "my fav number is:" , num1 )
+# print( f"my fav number is : {num1}")
+
+
+# name = "umar zahoor"
+# age = 24
+# fav_color = "black"
+
+# print(f"my name is {name} my age is {age} my fav_color is {fav_color}")
+
+# myName = "codepro"
+# Myname = "umar zahoor"
+# myName = "unknown"
+# print( myName)
+ 
+# myName ="umar zahoor"
+# surname= "raja g"
+# myName, surname= surname, myName
+# print(myName)
+# print(surname)
+
+
+
+# myName = "umar zahoor"
+# surname= "raja g"
+# fullName = myName +" "+ surname
+# print(fullName)
